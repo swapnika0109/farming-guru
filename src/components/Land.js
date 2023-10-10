b239408d004f8a5e6fe19985655a3be481f255dd
swapnika-aa <swapnikamaddimsetty@airasia.com>
2023-10-10 12:34:03 +0800

    Added blocks on the geometry

--- src/components/Land.js
+++ src/components/Land.js
@@ -1,10 +1,8 @@
-import React, {  useEffect } from 'react';
+import React, { useEffect } from 'react';
 import * as THREE from 'three';
-import image from '../assets/textures/land.jpg'
+import image from '../assets/textures/land.jpg';
 
 const Land = () => {
-
-
   useEffect(() => {
     // Set up the scene
     const scene = new THREE.Scene();
@@ -14,12 +12,39 @@ const Land = () => {
     document.body.appendChild(renderer.domElement);
 
     // Create land geometry and apply texture
-    const geometry = new THREE.PlaneGeometry(2,2,2,2); // Adjust the size as needed
+    const landGeometry = new THREE.PlaneGeometry(2, 2, 2, 2); // Adjust the size as needed
     const texture = new THREE.TextureLoader().load(image); // Path to your image
-    const material = new THREE.MeshBasicMaterial({  map: texture });
-    const land = new THREE.Mesh(geometry, material);
+    const landMaterial = new THREE.MeshBasicMaterial({ map: texture });
+    const land = new THREE.Mesh(landGeometry, landMaterial);
     land.rotation.set(-0.5, -0.1, 0.8);
     scene.add(land);
+
+    // Create block geometry and material
+    const blockGeometry = new THREE.PlaneGeometry(0.1, 0.1); // Adjust size as needed
+    const blockMaterial  = new THREE.LineBasicMaterial( {
+                          	color: 0xffffff,
+                          	linewidth: 1,
+                          	linecap: 'round', //ignored by WebGLRenderer
+                          	linejoin:  'round', //ignored by WebGLRenderer
+                          	transparent: true,  // Enable transparency
+                              opacity: 0.5
+                          } );
+
+    // Create and position two blocks
+    const block1 = new THREE.Mesh(blockGeometry, blockMaterial);
+    block1.position.set(0.2, 0.2, 0.1); // Adjust the position on the land
+     land.add(block1);
+    const block3 = new THREE.Mesh(blockGeometry, blockMaterial);
+        block3.position.set(0.4, 0.6, 0.1); // Adjust the position on the land
+    land.add(block3);
+
+    const block2 = new THREE.Mesh(blockGeometry, blockMaterial);
+    block2.position.set(-0.2, -0.2, 0.1); // Adjust the position on the land
+    land.add(block2);
+
+//    const plane = new THREE.Plane( new THREE.Vector3( 1, 1, 0.2 ), 3 );
+//    const helper = new THREE.PlaneHelper( plane, 1, 0xffff00 );
+//    scene.add( helper );
 
     // Position the camera
     camera.position.z = 5;
@@ -30,12 +55,23 @@ const Land = () => {
 
       // Rotate the land
       land.rotation.z += 0.0022;
-//      land.rotation.y += 0.01;
 
       renderer.render(scene, camera);
     };
 
     animate();
+
+    // Add click event listeners to blocks for interaction
+    const handleBlockClick = (block) => {
+      // Implement logic for block selection
+      console.log(`Selected block: ${block.name}`);
+    };
+
+    block1.name = 'Block 1';
+    block2.name = 'Block 2';
+
+    block1.addEventListener('click', () => handleBlockClick(block1));
+    block2.addEventListener('click', () => handleBlockClick(block2));
 
     // Clean up on unmount
     return () => {
