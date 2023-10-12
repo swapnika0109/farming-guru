a7e08a917fc23bf18ae9e2f8bb1ffffe3224db1b
swapnika-aa <swapnikamaddimsetty@airasia.com>
2023-10-12 13:16:37 +0800

    Aligned blocks inside geomtry

--- src/components/Land.js
+++ src/components/Land.js
@@ -10,36 +10,27 @@ const Land = () => {
   const mouse = useRef(new THREE.Vector2());
 
   useEffect(() => {
-    // Set up the scene
     scene.current = new THREE.Scene();
-    camera.current = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.9, 1000);
+    camera.current = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.9, 1000);
     renderer.current = new THREE.WebGLRenderer();
     renderer.current.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.current.domElement);
 
-    // Calculate the size of each flat box based on the total area and the number of boxes
-    const totalArea = 1800; // sqft
-    const numberOfBoxes = 5;
-    const boxSize = Math.sqrt(totalArea / numberOfBoxes);
+    let totalWidth = 5; // Initial size
+    let totalHeight = 7; // Initial size
 
-    const totalWidth = numberOfBoxes * boxSize / 100;
-    const totalHeight = numberOfBoxes * boxSize / 100;
+    const totalArea = totalWidth * totalHeight;
+    const numberOfBoxes = 6;
+    const boxSize = Math.sqrt(totalArea / numberOfBoxes);
     const texture = new THREE.TextureLoader().load(image);
 
-    // Create land geometry and apply texture
-    const landGeometry = new THREE.PlaneGeometry(totalWidth, totalHeight, 2, 2);
-    const landMaterial = new THREE.MeshBasicMaterial({ map: texture, transparent: true, opacity: 0.5 });
-    const land = new THREE.Mesh(landGeometry, landMaterial);
-    scene.current.add(land); // Add the land to the scene
-
-    // Function to create and add boxes to the land
     const createFlatBox = (position, parent) => {
       const boxGeometry = new THREE.BufferGeometry();
       const vertices = new Float32Array([
         -0.03, -0.02, 0, // bottom-left
-        totalWidth / 10, -0.02, 0, // bottom-right
-        totalWidth / 10, totalHeight / 10, 0, // top-right
-        -0.03, totalHeight / 10, 0, // top-left
+        totalWidth / numberOfBoxes, -0.02, 0, // bottom-right
+        totalWidth / numberOfBoxes, totalHeight / numberOfBoxes, 0, // top-right
+        -0.03, totalHeight / numberOfBoxes, 0, // top-left
       ]);
 
       const indices = new Uint16Array([0, 1, 2, 0, 2, 3]);
@@ -54,28 +45,48 @@ const Land = () => {
 
       const flatBox = new THREE.Mesh(boxGeometry, boxMaterial);
       flatBox.position.copy(position);
-      parent.add(flatBox); // Add the block as a child of the parent (land)
+      parent.add(flatBox);
 
       flatBox.userData.onClick = () => {
         console.log('Box clicked!');
       };
     };
 
+    // Create land geometry and apply texture
+    const landGeometry = new THREE.PlaneGeometry(totalWidth + 0.2, totalHeight + 0.2, 2, 2);
+    const landMaterial = new THREE.MeshBasicMaterial({ map: texture, transparent: true, opacity: 0.5 });
+    const land = new THREE.Mesh(landGeometry, landMaterial);
+    scene.current.add(land);
+
     let previousX = 0.0;
     let previousY = 0.0;
 
     // Calculate the positions for the flat boxes based on the land's dimensions
+//    for (let i = 0; i < numberOfBoxes; i++) {
+//      for (let j = 0; j < numberOfBoxes; j++) {
+//        const spacingX = totalWidth / numberOfBoxes;
+//        const spacingY = totalHeight / numberOfBoxes;
+//        const position = new THREE.Vector3(
+//          (i * spacingX - totalWidth / 2) + spacingX / 2,
+//          (j * spacingY - totalHeight / 2) + spacingY / 2,
+//          0.07
+//        );
+//        createFlatBox(position, land); // Pass the land as the parent
+//      }
+//    }
     for (let i = 0; i < numberOfBoxes; i++) {
       for (let j = 0; j < numberOfBoxes; j++) {
-        const spacing = totalWidth / numberOfBoxes;
+        const spacingX = totalWidth / numberOfBoxes;
+        const spacingY = totalHeight / numberOfBoxes;
         const position = new THREE.Vector3(
-          (i * spacing - totalWidth / 2) + spacing / 2,
-          (j * spacing - totalHeight / 2) + spacing / 2,
+          (i * spacingX - totalWidth / 2) + spacingX / 2 - totalWidth / (2 * numberOfBoxes),
+          (j * spacingY - totalHeight / 2) + spacingY / 2 - totalHeight / (2 * numberOfBoxes),
           0.07
         );
         createFlatBox(position, land); // Pass the land as the parent
       }
     }
+
 
     // Position the camera
     camera.current.position.z = 5;
